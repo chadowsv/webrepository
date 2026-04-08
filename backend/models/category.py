5091d960f51659a6bd8aa4b6a011b5564ffb567e
chadowsv <lisethsbv2013@gmail.com>
2026-04-08 09:55:57 -0500

    estoy cansada

--- backend/models/category.py
+++ backend/models/category.py
@@ -2,6 +2,6 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 class Category(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name: str
     description: Optional[str] = None
--- backend/models/product.py
+++ backend/models/product.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 class Product(SQLModel, table=True):
-    id:Optional[int] = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name:str
     description:Optional[str]
     price:float
